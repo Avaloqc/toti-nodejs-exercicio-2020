6525580649465d45e5de0c4846de64e4dd1ff7f7
Avaloqc <79610087+Avaloqc@users.noreply.github.com>
2021-09-06 11:52:34 -0400

    updating api routes

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs')
 
 // List tasks
 app.get('/tasks', async (req, res) => {
-  await tasks.findAll().then((fetchedData) => {
+  await tasks.findAll().then(fetchedData => {
     res.json(fetchedData)
   })
 })
@@ -27,31 +27,38 @@ app.get('/tasks', async (req, res) => {
 // Create task
 app.post('/tasks', async (req, res) => {
   const body = req.body
-  await tasks.create({description: body.description, done: body.done})
+  await tasks.create({ description: body.description, done: body.done })
   res.json(body)
-}) 
+})
 
-// Show task
+// Show task 
 app.get('/tasks/:id', async (req, res) => {
   const taskId = req.params.id
-  await tasks.findByPk(taskId).then((fetchedData) => {
-    res.json(fetchedData)
-  })
-  res.send({ action: 'Showing task', taskId: taskId })
+  await tasks.findByPk(taskId)
+    .then(fetchedData => {
+      res.status(200).json(fetchedData)
+      res.json(fetchedData)
+    })
+    /*.catch(err => res.status(500).json({
+      message: 'Essa tarefa não existe',
+      error: err
+    }))*/
 })
 
 // Update task
+
 app.put('/tasks/:id', async (req, res) => {
   const taskId = req.params.id
+  const body = req.body;
   const task = await tasks.findByPk(taskId)
-  task.update({description: body.description, done: body.done})
+  task.update({description: body.description, done: body.done })
   res.send({ action: 'Updating task', taskId: taskId })
 })
 
-// Delete task
+// Delete tasks
 app.delete('/tasks/:id', async (req, res) => {
   const taskId = req.params.id
-  await tasks.destroy({ where: { id: taskId} })
+  await tasks.destroy({ where: { id: taskId } })
   res.send({ action: 'Deleting task', taskId: taskId })
 })
 
