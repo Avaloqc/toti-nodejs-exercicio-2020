d6cc506caa684bb6d043694c316462dc530bf74d
Avaloqc <79610087+Avaloqc@users.noreply.github.com>
2021-09-05 22:14:47 -0400

    Solucao do exercicio

--- index.js
+++ index.js
@@ -9,36 +9,49 @@ const tasks = Task(sequelize, DataTypes)
 // We need to parse JSON coming from requests
 app.use(express.json())
 
+/*
+tasks.create({
+  description:'Cenoura',
+  done: false,
+})*/
+
+app.set('view engine', 'ejs')
+
 // List tasks
-app.get('/tasks', (req, res) => {
-  res.json({ action: 'Listing tasks' })
+app.get('/tasks', async (req, res) => {
+  await tasks.findAll().then((fetchedData) => {
+    res.json(fetchedData)
+  })
 })
 
 // Create task
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const body = req.body
-
+  await tasks.create({description: "Batata", done: true})
   res.json(body)
-})
+}) 
 
 // Show task
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const taskId = req.params.id
-
+  await tasks.findByPk(taskId).then((fetchedData) => {
+    res.json(fetchedData)
+  })
   res.send({ action: 'Showing task', taskId: taskId })
 })
 
 // Update task
-app.put('/tasks/:id', (req, res) => {
+app.put('/tasks/:id', async (req, res) => {
   const taskId = req.params.id
-
+  const task = await tasks.findByPk(taskId)
+  task.update({description: "Ananas", done: true})
   res.send({ action: 'Updating task', taskId: taskId })
 })
 
 // Delete task
-app.delete('/tasks/:id', (req, res) => {
+app.delete('/tasks/:id', async (req, res) => {
   const taskId = req.params.id
-
+  await tasks.destroy({ where: { id: taskId} })
   res.send({ action: 'Deleting task', taskId: taskId })
 })
 
